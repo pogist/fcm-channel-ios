e1b0bd861a21080114b4c5013d6cf5faead8dcd3
pogist <murilo.paixao.2@gmail.com>
2019-11-11 18:11:53 -0300

    fix: make FCMChannelError public

--- fcm-channel-ios/Classes/Model/FCMChannelError.swift
+++ fcm-channel-ios/Classes/Model/FCMChannelError.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum FCMChannelError: Error {
+public enum FCMChannelError: Error {
     case notFound(message: String?)
     case defaultError(message: String?)
     case mappingError
@@ -15,7 +15,7 @@ enum FCMChannelError: Error {
 }
 
 extension FCMChannelError: LocalizedError {
-    var errorDescription: String? {
+    public var errorDescription: String? {
         switch self {
         case .notFound(let message):
             return message ?? "Um erro ocorreu"
@@ -25,8 +25,6 @@ extension FCMChannelError: LocalizedError {
             return "Erro ao decodificar contato"
         case .noContact:
             return "Nenhum contato encontrado"
-        default:
-            return "Um erro ocorreu"
         }
     }
 }
